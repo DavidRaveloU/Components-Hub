e92467b703fbc057bae8a5b8e10d65ae25d58b5a
DavidRaveloU <dravelo@unicesar.edu.co>
2025-12-26 17:58:02 -0500

    feat: add index file for Expandable Cards Hero feature exports

---
+++ src/features/expandable-cards-hero/index.ts
@@ -0,0 +1,6 @@
+export { ExpandableCardsHero } from './components/ExpandableCardsHero';
+export { SongCard } from './components/SongCard';
+export { ExpandedCard } from './components/ExpandedCard';
+export { useExpandableCard } from './hooks/useExpandableCard';
+export { songs } from './data/songs';
+export type { Song, ExpandableCardsState } from './types/song.types';
